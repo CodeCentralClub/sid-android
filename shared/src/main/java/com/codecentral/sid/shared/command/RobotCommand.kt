29c2c323026a78f2b875956ab7675ad27ba95c9a
Willie Chalmers III <willieechalmers@gmail.com>
2018-09-26 14:00:01 -0600

    feat(commands): Begin creating system for remote commands

---
+++ shared/src/main/java/com/codecentral/sid/shared/command/RobotCommand.kt
@@ -0,0 +1,8 @@
+package com.codecentral.sid.shared.command
+
+/**
+ * A set of instructions to be run on the robot.
+ *
+ * TODO(command): Create a system for running remote commands
+ */
+data class RobotCommand(val instruction: String)
