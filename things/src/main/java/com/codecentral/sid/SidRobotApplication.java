53a28a0c917350431c9a9420f298d3438cc4ba4f
Willie Chalmers III <willieechalmers@gmail.com>
2018-09-27 13:24:31 -0600

    config(things): Update manifest to use SidBaseApplication subclass

---
+++ things/src/main/java/com/codecentral/sid/SidRobotApplication.java
@@ -0,0 +1,11 @@
+package com.codecentral.sid;
+
+import com.codecentral.sid.shared.SidBaseApplication;
+
+/**
+ * The application containing resources only the robot uses.
+ */
+public class SidRobotApplication extends SidBaseApplication {
+
+    // TODO(peripherals): Initialize PWMs, cameras, ultrasonic sensor, and controller accecss
+}
