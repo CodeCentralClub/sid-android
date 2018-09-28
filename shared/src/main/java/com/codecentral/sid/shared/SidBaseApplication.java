78084fb2c29569c44b7166e010e254639a678992
Willie Chalmers III <willieechalmers@gmail.com>
2018-09-28 09:26:28 -0600

    feat(nearby): Abstractify SidBaseApplication
     - Make subclasses return client nicknames
     - Make subclasses initialize Nearby

--- shared/src/main/java/com/codecentral/sid/shared/SidBaseApplication.java
+++ shared/src/main/java/com/codecentral/sid/shared/SidBaseApplication.java
@@ -2,30 +2,46 @@ package com.codecentral.sid.shared;
 
 import android.app.Application;
 import com.codecentral.sid.shared.nearby.NearbyRobotConnectionClient;
+import com.codecentral.sid.shared.nearby.RobotConnectionClient;
 
 /**
- * A common application class that initializes Nearby for the robot and
- * companion app.
+ * A common application class that initializes a companion app connection.
  */
-public class SidBaseApplication extends Application {
+public abstract class SidBaseApplication extends Application {
 
-    private NearbyRobotConnectionClient connectionClient;
+    private RobotConnectionClient connectionClient;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        connectionClient = new NearbyRobotConnectionClient(this);
+        connectionClient = createConnectionClient();
     }
+
+    /**
+     * Must be overridden in subclass to provide functionality.
+     *
+     * @return A RobotConnectionClient instance
+     * @see NearbyRobotConnectionClient
+     */
+    public abstract RobotConnectionClient createConnectionClient();
+
+    /**
+     * Must be overridden in subclass to provide functionality
+     *
+     * @return A unique client name for the type of device, like "Sid Companion
+     * App"
+     */
+    public abstract String getClientNickname();
 
     /**
      * Provides a {@link NearbyRobotConnectionClient} used to connect a robot to a
      * companion app.
-     *
+     * <p>
      * TODO(di): Use dependency injection to initialize this automatically
      *
      * @return This application's {@link NearbyRobotConnectionClient} instance
      */
-    public NearbyRobotConnectionClient getConnectionClient() {
+    public RobotConnectionClient getConnectionClient() {
         return connectionClient;
     }
 }
